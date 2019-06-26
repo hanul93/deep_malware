cb80f7c17988784e6da1b4813edf59251789ffaf
hanul93 <hanul93@gmail.com>
2019-06-27 06:53:32 +0900

    First Version

---
+++ #1.feature/crc16.py
@@ -0,0 +1,55 @@
+# -*- coding: utf8 -*-
+
+#
+# CRC16 MODULE
+#
+# includes CRC16 and CRC16 MODBUS
+#
+
+from ctypes import c_ushort
+
+
+class CRC16(object):
+    crc16_tab = []
+
+    # The CRC's are computed using polynomials. Here is the most used
+    # coefficient for CRC16
+    crc16_constant = 0xA001  # 40961
+
+    def __init__(self, modbus_flag=False):
+        # initialize the precalculated tables
+        if not len(self.crc16_tab):
+            self.init_crc16()
+        self.mdflag = bool(modbus_flag)
+
+    def calculate(self, input_data=None):
+        try:
+            is_string = isinstance(input_data, str)
+            is_bytes = isinstance(input_data, (bytes, bytearray))
+
+            if not is_string and not is_bytes:
+                raise Exception("Please provide a string or a byte sequence "
+                                "as argument for calculation.")
+
+            crc_value = 0x0000 if not self.mdflag else 0xffff
+
+            for c in input_data:
+                d = ord(c) if is_string else c
+                tmp = crc_value ^ d
+                rotated = crc_value >> 8
+                crc_value = rotated ^ self.crc16_tab[(tmp & 0x00ff)]
+
+            return crc_value
+        except Exception as e:
+            print("EXCEPTION(calculate): {}".format(e))
+
+    def init_crc16(self):
+        """The algorithm uses tables with precalculated values"""
+        for i in range(0, 256):
+            crc = c_ushort(i).value
+            for j in range(0, 8):
+                if crc & 0x0001:
+                    crc = c_ushort(crc >> 1).value ^ self.crc16_constant
+                else:
+                    crc = c_ushort(crc >> 1).value
+            self.crc16_tab.append(crc)
